f19958e2143e9e898e5da183aa4d039174ecd499
Benjamin <bjudkewitz@users.noreply.github.com>
2024-06-21 15:45:28 +0200

    python ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='arrayfifo',
-    version='0.0.1',
+    version='0.0.2',
     packages=[''],
     url='https://github.com/danionella/arrayfifo',
     license='MIT',
@@ -11,4 +11,13 @@ setup(
     description='Fast multiprocessing FIFO buffer (queue) for numpy arrays',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
